76400797cee69eef67b9735f85e6f1087831dfce
Charlotte Ellwood <char502@hotmail.com>
2018-05-08 22:30:31 +0100

    added html, css anf started js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+$(document).ready(function() {
+  $("#search").focus();
+  $("#search").off("keyup");
+  $("#search").on("keyup", function() {});
+});
 
-
-
-
-
+//fetch()`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=${resultsPage}&srsearch=${searchQuery}`;
