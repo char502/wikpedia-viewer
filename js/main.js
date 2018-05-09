631cddaa8e92444715d774ca0d96ad08fd948d01
Charlotte Ellwood <char502@hotmail.com>
2018-05-09 20:44:11 +0100

    added live search

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
+function ajaxCall() {
+  $.ajax({
+    url:
+      "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" +
+      $("#search").val(),
+    dataType: "jsonp",
+    type: "GET",
+    success: function(data) {
+      $("#update").empty();
+      var data = JSON.stringify(data);
+      data = JSON.parse(data);
+      //console.log(data);
+      console.log(data.query.search);
+      //loop through data and output
+      var output = "";
+      data.query.search.forEach(function(data) {
+        var title = "<h2>" + data.title + "</h2>" + "<br>";
+        var snippet = "<p>" + data.snippet + "</p>";
+        var url =
+          '<a href="https://en.wikipedia.org/wiki/' +
+          data.title +
+          '" target=_blank">';
+        var endUrl = "</a>";
+        output += url + title + endUrl + snippet + "<hr>";
+      });
+      $("#update").append(output);
+    },
+    error: function() {
+      alert("the data could not load");
+    }
+  });
+}
+
 $(document).ready(function() {
   $("#search").focus();
   $("#search").off("keyup");
-  $("#search").on("keyup", function() {});
+  $("#search").on("keyup", function() {
+    ajaxCall();
+  });
 });
 
 //fetch()`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=${resultsPage}&srsearch=${searchQuery}`;
