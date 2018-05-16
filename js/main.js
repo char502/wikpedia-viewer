f98a8c13aafa89c46c441a210d2940665e7e3557
char502 <char502@hotmail.com>
2018-05-16 22:13:29 +0100

    code review

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 const form = document.querySelector(".searchForm");
 
+//adding an event listener to the form node
 form.addEventListener("submit", handleSubmit);
 
+//creating the handleSubmit function
 function handleSubmit(event) {
   event.preventDefault();
   const input = document.querySelector(".searchForm-input").value;
   const searchQuery = input.trim();
-  //console.log(searchQuery);
-  // call `fetchResults` function and pass it the `searchQuery`
+  //call fetchResults function and pass it to the searchquery
   fetchResults(searchQuery);
 }
-
+//call the wiki api
 function fetchResults(searchQuery) {
-  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-  //console.log(url);
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchQuery}`;
   fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      //console.log(data);
       const results = data.query.search;
       displayResults(results);
     })
@@ -31,15 +30,10 @@ function fetchResults(searchQuery) {
 const resultPlace = document.querySelector(".searchResults");
 
 function displayResults(results) {
-  //console.log(results);
-
-  // remove all child elements
+  //make sure there is nothing in the search results div
   resultPlace.innerHTML = "";
 
   results.forEach(function(result) {
-    //console.log(element);
-    // const newTitle = result.title;
-    // console.log(newTitle);
     const resultLink = encodeURI(
       `https://en.wikipedia.org/wiki/${result.title}`
     );
@@ -48,121 +42,25 @@ function displayResults(results) {
       "beforeend",
       `
     <div class="resultItem">
-      <h3 class="resultITem-title">
+      <h3 class="resultItem-title">
         <a href="${resultLink}" target="_blank" rel="noopener">${
         result.title
       }</a>
       </h3>
       <span class="resultItem-snippet">${result.snippet}</span><br>
-      <a href="${resultLink}" class="resultITem-link" target="_blank" rel="noopener noreferrer">
+      <a href="${resultLink}" class="resultItem-link" target="_blank" rel="noopener noreferrer">
     </div>`
     );
   });
 }
 
-resultPlace.addEventListener("keyup", function() {
-  displayResults(results);
-});
-
-// const eventList = ["focus", "keyup"];
-// for (event of eventList) {
-//   resultPlace.addEventListener(event, function() {
-//     // your function body...
-//     displayResults(results);
-//   });
-// }
-
-// resultPlace.addEventListener(function() {
-//   resultPlace.focus();
-//   resultPlace.onkeyup() = null;
-//   resultPlace.onkeyup(function () {
-//     displayResults();
-//   })
-
-// });
-
-// $(document).ready(function() {
-//   $(".searchResults").focus();
-//   $(".searchResults").off("keyup");
-//   $(".searchResults").on("keyup", function() {
-//     displayResults();
-//   });
-// });
-
-// const resOnPage = function(finalData) {
-//   finalData = data.query.search;
-//   console.log(finalData);
-//   for (let i in data) {
-//     const displaySearchResult = document.querySelector(".searchResult");
-//     const newElement = displaySearchResult.createElement("p");
-//     //newElement.id = data[i];
-//     //newElement.className = "SearchResultItem";
-//     newElement.immerHTML = data[i];
-//     displaySearchResult.appendChild(newElement);
-//   }
-// };
-
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//     })
-//     .catch(() => console.log("An Error occured"));
-// }
-
-// To get a random wiki article on button click
-
-const button = document.getElementById("randomButton");
+// Click a button to get random Wiki article
+const button = document.querySelector("#randomButton");
 
 button.addEventListener("click", function() {
   window.open(
     "https://en.wikipedia.org/wiki/Special:Random",
-    (target = "_blank"),
+    (target = "blank"),
     (rel = "noopener noreferrer")
   );
 });
-// const newWnd = window.open();
-// newWnd.opener = null;
-
-// function ajaxCall() {
-//   $.ajax({
-//     url:
-//       "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" +
-//       $("#search").val(),
-//     dataType: "jsonp",
-//     type: "GET",
-//     success: function(data) {
-//       $("#update").empty();
-//       var data = JSON.stringify(data);
-//       data = JSON.parse(data);
-//       //console.log(data);
-//       console.log(data.query.search);
-//       //loop through data and output
-//       var output = "";
-//       data.query.search.forEach(function(data) {
-//         var title = "<h2>" + data.title + "</h2>" + "<br>";
-//         var snippet = "<p>" + data.snippet + "</p>";
-//         var url =
-//           '<a href="https://en.wikipedia.org/wiki/' +
-//           data.title +
-//           '" target=_blank">';
-//         var endUrl = "</a>";
-//         output += url + title + endUrl + snippet + "<hr>";
-//       });
-//       $("#update").append(output);
-//     },
-//     error: function() {
-//       alert("the data could not load");
-//     }
-//   });
-// }
-
-// $(document).ready(function() {
-//   $("#search").focus();
-//   $("#search").off("keyup");
-//   $("#search").on("keyup", function() {
-//     ajaxCall();
-//   });
-// });
-
-//fetch()`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=${resultsPage}&srsearch=${searchQuery}`;
